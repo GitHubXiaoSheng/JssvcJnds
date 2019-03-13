e094bc98d43a67ec1ae60a9f5ee77b96d3e1f2b6
Zzj <1471888328@qq.com>
2019-03-13 11:11:43 +0800

    zzj

--- app/src/main/java/com/app/trafficclient/MySQLiteOpenHelper.java
+++ app/src/main/java/com/app/trafficclient/MySQLiteOpenHelper.java
@@ -10,11 +10,26 @@ public class MySQLiteOpenHelper extends SQLiteOpenHelper {
     public static final String DBNAME = "sql.db";
 
     public Context contextId;
+
     public static final String CHONGZHI_JILU = "create table chongzhi("
             + "id Integer primary key autoincrement,"
             + "chehao Integer,"
             + "jine Integer,"
             + "caozuoren text,"
+            + "time text)";
+
+    public static final String HUANJING_ZHIBIAO = "create table huanjing("
+            + "id Integer primary key autoincrement,"
+            + "wendu Integer,"
+            + "shidu Integer,"
+            + "guangzhao Integer,"
+            + "co2 Integer,"
+            + "pm25 Integer,"
+            + "time text)";
+
+    public static final String DAOLU_ZHIBIAO = "create table daolu("
+            + "id Integer primary key autoincrement,"
+            + "daolu Integer,"
             + "time text)";
 
     public MySQLiteOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
@@ -25,6 +40,8 @@ public class MySQLiteOpenHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CHONGZHI_JILU);
+        db.execSQL(HUANJING_ZHIBIAO);
+        db.execSQL(DAOLU_ZHIBIAO);
     }
 
     @Override
