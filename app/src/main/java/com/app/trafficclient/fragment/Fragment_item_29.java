a1ad9cf96caf32c0432d62ac0553721c3a0ca5c5
yws <1196201776@qq.com>
2019-03-25 18:26:26 +0800

    yws

--- app/src/main/java/com/app/trafficclient/fragment/Fragment_item_29.java
+++ app/src/main/java/com/app/trafficclient/fragment/Fragment_item_29.java
@@ -1,6 +1,10 @@
 package com.app.trafficclient.fragment;
 
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -15,6 +19,7 @@ import com.app.trafficclient.util.HttpRequest;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.AxisBase;
 import com.github.mikephil.charting.components.XAxis;
+import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
@@ -24,20 +29,31 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
+import java.util.Timer;
+import java.util.TimerTask;
+
+import static android.os.Build.VERSION_CODES.N;
 
 /**
  * A simple {@link Fragment} subclass.
  */
-public class Fragment_item_29 extends Fragment {
+public class Fragment_item_29 extends Fragment implements View.OnClickListener {
     private TextView show_zhishu;
     private LineChart pm2lineChart;
     private LinearLayout huadongbuju;
     private LineDataSet lineDataSet;
     private LineData lineData;
     private List<Entry> pm2Entry;
+    private TextView yuan1,yuan2,yuan3;
+    List<Integer>colors=new ArrayList<>();
+    private NotificationManager notificationManager;
+    private Notification notification;
 
 
+    private int index = 0;
 
 
     @Override
@@ -51,15 +67,41 @@ public class Fragment_item_29 extends Fragment {
         super.onStart();
         show_zhishu=getActivity().findViewById(R.id.tv_show_zhishu);
         pm2lineChart=getActivity().findViewById(R.id.line_chart);
+
+
         huadongbuju=getActivity().findViewById(R.id.huadongbuju);
         pm2lineChart=getActivity().findViewById(R.id.line_chart);
-        http();
-        lineDataSet=new LineDataSet(pm2Entry,"");
-        lineData=new LineData(lineDataSet);
-        pm2lineChart.setData(lineData);
-        pm2lineChart.invalidate();
+        yuan1=getActivity().findViewById(R.id.yuan_pm2);
+        yuan2=getActivity().findViewById(R.id.yuan_wendu);
+        yuan3=getActivity().findViewById(R.id.yuan_guangzhao);
+        yuan1.setOnClickListener(this);
+        yuan2.setOnClickListener(this);
+        yuan3.setOnClickListener(this);
 
+        pm2Entry=new ArrayList<>();
+        for (int i=0;i<5;i++){
+            pm2Entry.add(new Entry(i,new Random().nextInt(200)));
+        }
 
+         fangfa();
+        yuan1.setBackgroundDrawable(getResources().getDrawable(R.drawable.zzj_background_2));
+
+        colors.add(Color.RED);
+        colors.add(Color.RED);
+        colors.add(Color.RED);
+        colors.add(Color.RED);
+        colors.add(Color.RED);
+        setcolor();
+        shuanxin();
+
+//        for (int i = 0; i < pm2Entry.size(); i++) {
+//            if (pm2Entry.get(i).getY() > 150){
+//                index = i;
+//            }
+//        }
+//
+//        colors.set(index,Color.RED);
+//        lineDataSet.setColors(colors);
 
         XAxis xAxis=pm2lineChart.getXAxis();
         xAxis.setValueFormatter(new IAxisValueFormatter() {
@@ -69,24 +111,144 @@ public class Fragment_item_29 extends Fragment {
             }
         });
 
+
+        xAxis.setDrawAxisLine(true);
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM);
+        xAxis.setDrawGridLines(false);
+        xAxis.setDrawLabels(false);
+        xAxis.setLabelCount(6,true);
+        YAxis yAxis_left=pm2lineChart.getAxisLeft();
+        YAxis yAxis_right=pm2lineChart.getAxisRight();
+        yAxis_left.setDrawAxisLine(true);
+        yAxis_right.setDrawAxisLine(false);
+        yAxis_right.setDrawLabels(false);
+        yAxis_left.setDrawLabels(true);
+        yAxis_right.setDrawGridLines(false);
+        yAxis_left.setDrawGridLines(false);
+
+
+
+
+
+        notificationManager= (NotificationManager)getActivity().getSystemService(Context.NOTIFICATION_SERVICE);
+        notification=new Notification.Builder(getContext())
+                .setContentTitle("")
+                .setContentText("")
+                .setWhen(System.currentTimeMillis())
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .build();
+        notificationManager.notify(1,notification);
+
+
+
+
+
+
+    }
+    private void setcolor(){
+
+        for (int i = 0; i < pm2Entry.size(); i++) {
+            if (pm2Entry.get(i).getY() > 100){
+                index = i;
+            }
+        }
+
+        colors.set(index,Color.RED);
+        lineDataSet.setCircleColors(colors);
+
+    }
+    private void fangfa(){
+        lineDataSet=new LineDataSet(pm2Entry,"");
+        lineDataSet.setCircleColor(Color.GREEN);
+        lineDataSet.setDrawCircleHole(false);
+
+        lineData=new LineData(lineDataSet);
+        lineData.setDrawValues(false);
+        pm2lineChart.setData(lineData);
+        pm2lineChart.setDescription(null);
+        pm2lineChart.getLegend().setFormSize(0);
+        pm2lineChart.invalidate();
+
     }
 
-    int i = 0;
-    private void  http(){
-        HttpRequest.post("GetAllSense", "{\"UserName\":\"user1\"}", new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject jsonObject) {
-                try {
-                    if (i < 5){
-                        pm2Entry.add(new Entry(i,Float.valueOf(jsonObject.getString("pm2.5"))));
-                    }else {
-                        i = -1;
-                    }
-                    i ++;
-                } catch (JSONException e) {
-                    e.printStackTrace();
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.yuan_pm2:
+                show_zhishu.setText("pm2.5指数");
+                yuan1.setBackgroundDrawable(getResources().getDrawable(R.drawable.zzj_background_2));
+                yuan2.setBackgroundDrawable(getResources().getDrawable(R.drawable.zl_frag6_cboder_2));
+                yuan3.setBackgroundDrawable(getResources().getDrawable(R.drawable.zl_frag6_cboder_2));
+                pm2Entry.clear();
+                shuanxin();
+                for (int i=0;i<5;i++){
+                    pm2Entry.add(new Entry(i,new Random().nextInt(200)));
                 }
+            setcolor();
+                fangfa();
+
+                break;
+            case R.id.yuan_wendu:
+                show_zhishu.setText("光照强度指数");
+                yuan2.setBackgroundDrawable(getResources().getDrawable(R.drawable.zzj_background_2));
+                yuan1.setBackgroundDrawable(getResources().getDrawable(R.drawable.zl_frag6_cboder_2));
+                yuan3.setBackgroundDrawable(getResources().getDrawable(R.drawable.zl_frag6_cboder_2));
+                pm2Entry.clear();
+                shuanxin();
+                for (int i=0;i<5;i++){
+                    pm2Entry.add(new Entry(i,new Random().nextInt(100*2)));
+                }
+                setcolor();
+                fangfa();
+
+                break;
+            case R.id.yuan_guangzhao:
+                show_zhishu.setText("空气温度指数");
+                yuan3.setBackgroundDrawable(getResources().getDrawable(R.drawable.zzj_background_2));
+                yuan2.setBackgroundDrawable(getResources().getDrawable(R.drawable.zl_frag6_cboder_2));
+                yuan1.setBackgroundDrawable(getResources().getDrawable(R.drawable.zl_frag6_cboder_2));
+                pm2Entry.clear();
+                for (int i=0;i<5;i++){
+                    pm2Entry.add(new Entry(i,new Random().nextInt(300*3+5)));
+                }
+                fangfa();
+               setcolor();
+                shuanxin();
+                break;
+                default:
+                    break;
+        }
+    }
+
+//    int i = 0;
+//    private void  http(){
+//        HttpRequest.post("GetAllSense", "{\"UserName\":\"user1\"}", new Response.Listener<JSONObject>() {
+//            @Override
+//            public void onResponse(JSONObject jsonObject) {
+//                try {
+//                    if (i < 5){
+//                        pm2Entry.add(new Entry(i,Float.valueOf(jsonObject.getString("pm2.5"))));
+//                    }else {
+//                        i = -1;
+//                    }
+//                    i ++;
+//                } catch (JSONException e) {
+//                    e.printStackTrace();
+//                }
+//            }
+//        },null);
+//    }
+    private void shuanxin(){
+        Timer timer=new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                pm2Entry.clear();
+                for (int i=0;i<5;i++){
+                    pm2Entry.add(new Entry(i,new Random().nextInt(100*3)));
+                }
+                fangfa();
             }
-        },null);
+        },1,3000);
     }
 }
--- app/src/main/java/com/app/trafficclient/fragment/Fragment_item_31.java
+++ app/src/main/java/com/app/trafficclient/fragment/Fragment_item_31.java
@@ -1,29 +1,24 @@
 package com.app.trafficclient.fragment;
 
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 
+import com.app.trafficclient.MySQLiteOpenHelper;
 import com.app.trafficclient.R;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class Fragment_item_31 extends Fragment {
-
-
-    public Fragment_item_31() {
-        // Required empty public constructor
-    }
 
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_layout_item_31, container, false);
     }
 
