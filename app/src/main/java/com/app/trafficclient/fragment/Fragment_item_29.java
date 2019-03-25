06c5380357de9774d217d15bbeb131e350a8fbd9
Zzj <1471888328@qq.com>
2019-03-25 14:07:30 +0800

    zzj

--- app/src/main/java/com/app/trafficclient/fragment/Fragment_item_29.java
+++ app/src/main/java/com/app/trafficclient/fragment/Fragment_item_29.java
@@ -2,6 +2,7 @@ package com.app.trafficclient.fragment;
 
 
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,80 +14,78 @@ import com.android.volley.Response;
 import com.app.trafficclient.R;
 import com.app.trafficclient.util.HttpRequest;
 import com.github.mikephil.charting.charts.LineChart;
-import com.github.mikephil.charting.components.AxisBase;
-import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
-/**
- * A simple {@link Fragment} subclass.
- */
 public class Fragment_item_29 extends Fragment {
     private TextView show_zhishu;
-    private LineChart pm2lineChart;
     private LinearLayout huadongbuju;
+
+    private LineChart lineChart;
+    private List<Entry> entryList = new ArrayList<>();
     private LineDataSet lineDataSet;
     private LineData lineData;
-    private List<Entry> pm2Entry;
 
-
-
+    private Timer timer;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_layout_item_29, container, false);
     }
 
     @Override
-    public void onStart() {
-        super.onStart();
-        show_zhishu=getActivity().findViewById(R.id.tv_show_zhishu);
-        pm2lineChart=getActivity().findViewById(R.id.line_chart);
-        huadongbuju=getActivity().findViewById(R.id.huadongbuju);
-        pm2lineChart=getActivity().findViewById(R.id.line_chart);
-        http();
-        lineDataSet=new LineDataSet(pm2Entry,"");
-        lineData=new LineData(lineDataSet);
-        pm2lineChart.setData(lineData);
-        pm2lineChart.invalidate();
-
-
-
-        XAxis xAxis=pm2lineChart.getXAxis();
-        xAxis.setValueFormatter(new IAxisValueFormatter() {
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        initView();
+        timer = new Timer();
+        timer.schedule(new TimerTask() {
             @Override
-            public String getFormattedValue(float v, AxisBase axisBase) {
-                return null;
+            public void run() {
+                addData(1);
             }
-        });
+        },0,3000);
+    }
+    private int i = 0;
+    private void addData(int in){
+        if (in == 1) {
+            HttpRequest.post("GetAllSense", "{\"UserName\":\"user1\"}", new Response.Listener<JSONObject>() {
+                @Override
+                public void onResponse(JSONObject jsonObject) {
+                    try {
+                        if (i < 5) {
+                            entryList.add(new Entry(i++,jsonObject.getInt("pm2.5")));
+                            lineDataSet = new LineDataSet(entryList,"");
+                            lineData = new LineData(lineDataSet);
+                            lineChart.setData(lineData);
+                            lineChart.invalidate();
+                        }else {
 
+                            i = -1;
+                        }
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+                }
+            },null);
+
+        }
     }
 
-    int i = 0;
-    private void  http(){
-        HttpRequest.post("GetAllSense", "{\"UserName\":\"user1\"}", new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject jsonObject) {
-                try {
-                    if (i < 5){
-                        pm2Entry.add(new Entry(i,Float.valueOf(jsonObject.getString("pm2.5"))));
-                    }else {
-                        i = -1;
-                    }
-                    i ++;
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        },null);
+
+    private void initView() {
+        show_zhishu = getActivity().findViewById(R.id.tv_show_zhishu);
+        huadongbuju = getActivity().findViewById(R.id.huadongbuju);
+
+        lineChart = getActivity().findViewById(R.id.yws_line_chart);
+
     }
 }
