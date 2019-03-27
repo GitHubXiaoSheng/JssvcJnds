3ce8887c4538b95b28d5cf4bf5705cedd44f52d3
Zzj <1471888328@qq.com>
2019-03-27 15:02:18 +0800

    zzj

---
+++ app/src/main/java/com/app/trafficclient/fragment/Fragment_item_46.java
@@ -0,0 +1,30 @@
+package com.app.trafficclient.fragment;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.app.trafficclient.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Fragment_item_46 extends Fragment {
+
+
+    public Fragment_item_46() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_item_layout_46, container, false);
+    }
+
+}
